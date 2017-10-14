e1430571a4047314d88dd7254867d292b39af8b8
Thibaut Colar <tcolar@colar.net>
2017-10-14 13:07:07 -0700

    Fix build.sh Go cross-compilation command

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 # Build linux x64 binary, to be used by docker container
+rm -rf build/
 mkdir -p ./build/linux_amd64/
 binpath=./build/linux_amd64/convoy
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o $binpath || exit 1
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-s" \
+  -a -installsuffix cgo -o $binpath ./cmd/convoy/ || exit 1
 chmod +x $binpath
